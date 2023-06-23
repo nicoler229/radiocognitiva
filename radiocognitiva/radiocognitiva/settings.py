37d4bc31ba0c8500fce9b7fa7f6a9fa59c5ebd81
Nicole Ramos <segundoue27@gmail.com>
2023-06-23 14:27:59 -0400

    2023-06-23

--- radiocognitiva/radiocognitiva/settings.py
+++ radiocognitiva/radiocognitiva/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,12 +75,9 @@ WSGI_APPLICATION = 'radiocognitiva.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'radiocognitiva_database',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'root'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    
     }
 }
 
