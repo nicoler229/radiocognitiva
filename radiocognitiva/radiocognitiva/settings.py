017e19d32e8c3299823d9a9159dc372ed2b42e70
Nicole Ramos <segundoue27@gmail.com>
2023-06-23 10:49:58 -0400

    2023-06-23

--- radiocognitiva/radiocognitiva/settings.py
+++ radiocognitiva/radiocognitiva/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'radiocognitiva.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'radiocognitiva_database',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'root'
     }
 }
 
